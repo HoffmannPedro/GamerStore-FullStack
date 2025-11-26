4e260006ed7c2b7fbb1d2fc8d6a9f3dc639896bb
Pedro <hoffmannpedro18@gmail.com>
2025-11-26 15:43:49 -0300

    FIX:Variables de entorno para Cloudinary

--- client/src/components/admin/ProductForm.jsx
+++ client/src/components/admin/ProductForm.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 import api from '../../services/api';
